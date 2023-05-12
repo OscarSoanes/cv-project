3220de8f989346e27c26ffdb6d1deb3af2fc8f8f
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-12 23:25:37 +0100

    Added details to be displayed

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,13 @@
-export function Details() {
+import { Input } from "./Input"
+
+export function Details({dataKey, inputs, inputHandler, removeHandler}) {
     return (
-        <>
-            <p>Hello</p>
-        </>
+        <div>
+            {inputs.map((input) => 
+                <Input key={input.id} id={input.id} onChange={inputHandler} prettier={input.prettier} parent={dataKey}></Input>
+            )}
+
+            <button onClick={() => removeHandler(dataKey)}>Remove</button>
+        </div>
     )
 }
