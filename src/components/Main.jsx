2b8133f2217741559da5c3c4c751a980a9f57666
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-12 20:20:31 +0100

    Added default JSON data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import defaultData from "../default.json";
+
 export function Main() {
+    const [data, setData] = useState(defaultData)
+
     return (
         <main>
             <h2>Edit</h2>
