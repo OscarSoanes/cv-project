1a1f672205322a5584e51154d93b56b93a18fccf
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-13 13:51:05 +0100

    Date picker added to JSX, set up for month date

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,24 @@
 import { Input } from "./Input"
+import Picker from "react-datepicker"
+import { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
 
 export function Details({dataKey, inputs, inputHandler, removeHandler}) {
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
         <div>
-            {inputs.map((input) => 
-                <Input key={input.id} id={input.id} onChange={inputHandler} prettier={input.prettier} parent={dataKey}></Input>
-            )}
+            {inputs.map((input) => {
+                if (input.id.includes("Date")) {
+                    return (<Picker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        dateFormat="MM/yyyy"
+                        showMonthYearPicker
+                        />)
+                }
+                return <Input></Input>
+            })}       
 
             <button onClick={() => removeHandler(dataKey)}>Remove</button>
         </div>
