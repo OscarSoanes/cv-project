32a09412bbbbd4cfc81ec833462e3242e3c80743
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-14 16:48:57 +0100

    Designed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
+import { AiFillGithub } from 'react-icons/ai'
+
 export function Footer() {
     return (
-        <footer>
-            <p>Built by </p>
-            <a href="https://github.com/OscarSoanes">Oscar Soanes</a>
+        <footer className="flex gap-1 justify-center content-center items-center mt-12 p-2 rounded-t-lg bg-slate-700 text-gray-100">
+            <p>Built by</p>
+            <a className="flex gap-3 underline hover:text-sky-300" href="https://github.com/OscarSoanes">Oscar Soanes <AiFillGithub className="h-6 w-6"/> </a>
         </footer>
     )
 }
