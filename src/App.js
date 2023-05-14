abe41b212a2612b5f1eb8a8881c19834066e4289
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-14 16:53:36 +0100

    Made heading font larger by media query

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Footer } from "./components/Footer";
 function App() {
   return (
     <div className="page h-full">
-      <h1 className="text-center text-3xl font-medium tw mt-3">CV Builder</h1>
+      <h1 className="text-center text-3xl font-medium tw mt-3 sm:text-5xl">CV Builder</h1>
       <Main />
       <Footer />
     </div>
