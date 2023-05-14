1048ca3be8b819ebfe6a496d223d8c8ddcc9a00f
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-14 12:18:14 +0100

    Styling for each big section on CV

--- src/components/previewSection/DetailedSection.jsx
+++ src/components/previewSection/DetailedSection.jsx
@@ -3,7 +3,7 @@ import { Row } from "./Row"
 export function DetailedSection({type, array}) {
     return (
         <section>
-            <h3>{type}</h3>
+            <h3 className="text-[2.5vw] lg:text-[1.75vw] 2xl:text-[1.25vw]">{type}</h3>
 
             {array.map((field) => 
                 <Row key={field.id} data={field}/>
--- src/components/previewSection/Row.jsx
+++ src/components/previewSection/Row.jsx
@@ -1,12 +1,12 @@
 export function Row({data}) {
     return (
-        <section>
-            <div>
-                <h4>{data.position || data.course} </h4>
+        <section className="border-b border-solid border-black pb-1 text-[1.75vw] lg:text-[1.25vw] 2xl:text-[0.75vw] mb-3">
+            <div className="flex justify-between items-center">
+                <h4 className="font-bold text-[2vw] lg:text-[1.5vw] 2xl:text-[1vw]">{data.position || data.course} </h4>
 
-                <aside>
+                <aside className="flex gap-1">
                     <p>{data.company || data.university} |</p>
-                    <p>
+                    <p className="font-bold">
                         {data.startDate + " - "}
                         {data.endDate === "" ? "Present" : data.endDate}
                     </p>
