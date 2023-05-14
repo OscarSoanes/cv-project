3e2f36e68bcd11ac7614ec17c6ac421561ae8a4b
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-14 03:03:31 +0100

    Basic page style some minor styling to heading

--- src/components/previewSection/Preview.jsx
+++ src/components/previewSection/Preview.jsx
@@ -4,26 +4,28 @@ import { DetailedSection } from "./DetailedSection"
 export function Preview({data}) {
     return (
         <section>
-            <h2>Preview</h2>
+            <h2 className="mt-3 mb-3 text-2xl font-normal">Preview</h2>
 
-            <section>
-                <div>
-                    <h3>{data.name}</h3>
-                    <p>{data.position}</p>
-                </div>
-                <aside>
-                    <SmallContent value={data.phone}/>
-                    <SmallContent value={data.email}/>
-                    <SmallContent value={data.location}/>
-                </aside>
-            </section>
+            <div className="bg-white p-6 px-[10%] border-black border border-solid shadow shadow-gray-400 aspect-[1/1.414]">
+                <section className="border-b-4 border-solid border-teal-400 header">
+                    <div>
+                        <h3 className="font-bold text-teal-800 sm:text-2xl">{data.name}</h3>
+                        <p className="font-bold text-teal-800 sm:text-2xl">{data.position}</p>
+                    </div>
+                    <aside>
+                        <SmallContent value={data.phone}/>
+                        <SmallContent value={data.email}/>
+                        <SmallContent value={data.location}/>
+                    </aside>
+                </section>
 
-            <section>
-                <p>{data.description}</p>
-            </section>
+                <section>
+                    <p>{data.description}</p>
+                </section>
 
-            <DetailedSection type="Work Experience" array={data.workExperience} />
-            <DetailedSection type="Education" array={data.education} />
+                <DetailedSection type="Work Experience" array={data.workExperience} />
+                <DetailedSection type="Education" array={data.education} />
+            </div>
         </section>
     )
 }
