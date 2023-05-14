c14e1e47369ce46e6d5a26fb9301775eafe12675
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-14 12:18:04 +0100

    Added styling on the smaller information in heading

--- src/components/previewSection/Preview.jsx
+++ src/components/previewSection/Preview.jsx
@@ -1,25 +1,27 @@
 import { SmallContent } from "./SmallContent"
 import { DetailedSection } from "./DetailedSection"
 
+import { MdPhone, MdEmail, MdLocationPin } from 'react-icons/md';
+
 export function Preview({data}) {
     return (
         <section>
             <h2 className="mt-3 mb-3 text-2xl font-normal">Preview</h2>
 
             <div className="bg-white p-6 px-[10%] border-black border border-solid shadow shadow-gray-400 aspect-[1/1.414]">
-                <section className="border-b-4 border-solid border-teal-400 header">
+                <section className="border-b-4 border-solid border-teal-400 header pb-3 min-h-[11vh]">
                     <div>
-                        <h3 className="font-bold text-teal-800 text-[4vw] lg:text-[3vw]">{data.name}</h3>
-                        <p className="font-bold text-teal-800 text-[3vw] lg:text-[2vw]">{data.position}</p>
+                        <h3 className="font-bold text-teal-800 text-[4vw] lg:text-[3vw] align-top 2xl:text-[2vw]">{data.name}</h3>
+                        <p className="font-bold text-teal-800 text-[3vw] lg:text-[2vw] 2xl:text-[1.25vw]">{data.position}</p>
                     </div>
-                    <aside>
-                        <SmallContent value={data.phone}/>
-                        <SmallContent value={data.email}/>
-                        <SmallContent value={data.location}/>
+                    <aside className="grid justify-items-end content-center">
+                        <SmallContent value={data.phone} icon={<MdPhone className="w-[1.5vw] lg:w-[1vw] 2xl:text-[0.75vw]"/>}/>
+                        <SmallContent value={data.email} icon={<MdEmail className="w-[1.5vw] lg:w-[1vw] 2xl:text-[0.75vw]"/>}/>
+                        <SmallContent value={data.location} icon={<MdLocationPin className="w-[1.5vw] lg:w-[1vw] 2xl:text-[0.75vw]"/>}/>
                     </aside>
                 </section>
 
-                <section>
+                <section className="my-4 text-[2vw] lg:text-[1.25vw] 2xl:text-[1vw]">
                     <p>{data.description}</p>
                 </section>
 
--- src/components/previewSection/SmallContent.jsx
+++ src/components/previewSection/SmallContent.jsx
@@ -1,8 +1,8 @@
-export function SmallContent({img, value}) {
+export function SmallContent({value, icon}) {
     return (
-        <div>
-            <image></image>
-            <p>{value}</p>
+        <div className="flex justify-end gap-1 items-center">
+            {icon}
+            <p className="text-[2vw] lg:text-[1vw] 2xl:text-[0.75vw]">{value}</p>
         </div>
     )
 }
